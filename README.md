1b5f3aeea5e8d956a2ad0bfd43e914a4e6c0ab48
Matthias Endler <matthias-endler@gmx.net>
2012-03-07 16:14:14 +0100

    Updating client for use with DynDns.
    rc="$(curl $curl_verbose $curlopt -A "$version" -u "$user:$pw" \
                 'https://members.dyndns.org/nic/update?'"$param")"

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
-# zerigo dyndns client in shell script
-This is a simple dns update client for zerigo. It uses
+# lamedns.sh
+This is a simple dns update client for DynDns. It uses
 [icanhazip.com](http://icanhazip.com) for checking the external address and
 saves it to a textfile. The textfile is checked and updates are only done if
 the IP has actually changed.
 
 ## Files
-- `zerigo_dyndns.sh` is the actual script to execute
-- `launch.plist` is a launchd plist file to run the shell script once per hour
+- `lamedns.sh` is the actual script to execute
+- `de.matthias-endler.lamedns.plist` is a launchd plist file to run the shell script once per hour
+
+## Credits
+Forked from mrtazz/zerigo_dyndns.sh. Minor adjustments to make it work
+for DynDns.
--- lamedns.sh
+++ lamedns.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 #
 # shell script to check if external IP has changed and
-# update zerigo account if so
+# update DynDns account if so
 #
 
-# login user and update token
+# login user and password
 USER=""
-TOKEN=""
+PW=""
 # file where the last looked-up ip is stored
 IPFILE=""
-# basic zerigo update URL
-ZURL="http://update.zerigo.com/dynamic?"
-# the hostname to change with the new IP
+# basic DynDNS update URL
+DURL="https://members.dyndns.org/nic/update?'"
+# the hostname to change with the new IP eg.: test.dyndns.org
 HOST=""
 
 LAST_IP=`cat ${IPFILE} 2>/dev/null`
@@ -21,7 +21,7 @@ if [ "$LAST_IP" != "$CURRENT_IP" ]
 then
   echo "IP has changed to ${CURRENT_IP}."
   echo "Updating zerigo account..."
-  curl -s "${ZURL}host=${HOST}&ip=${CURRENT_IP}&user=${USER}&password=${TOKEN}"
+  curl -u "$USER:$PW" "${DURL}hostname=${HOST}"
   echo "\nDone."
   echo "${CURRENT_IP}" > ${IPFILE}
 fi
