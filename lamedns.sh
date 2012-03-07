bf8817fc7c989924ed456d17effdb22fac83d0b4
Matthias Endler <matthias-endler@gmx.net>
2012-03-07 16:29:55 +0100

    Fix typos.

--- lamedns.sh
+++ lamedns.sh
@@ -10,7 +10,7 @@ PW=""
 # file where the last looked-up ip is stored
 IPFILE=""
 # basic DynDNS update URL
-DURL="https://members.dyndns.org/nic/update?'"
+DURL="https://members.dyndns.org/nic/update?"
 # the hostname to change with the new IP eg.: test.dyndns.org
 HOST=""
 
@@ -20,7 +20,7 @@ CURRENT_IP=`curl -s http://icanhazip.com`
 if [ "$LAST_IP" != "$CURRENT_IP" ]
 then
   echo "IP has changed to ${CURRENT_IP}."
-  echo "Updating zerigo account..."
+  echo "Updating DynDNS account..."
   curl -u "$USER:$PW" "${DURL}hostname=${HOST}"
   echo "\nDone."
   echo "${CURRENT_IP}" > ${IPFILE}
